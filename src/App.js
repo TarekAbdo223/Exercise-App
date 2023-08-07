11e87da7d6c9fa23ff2da66ae80bde890ca69ca4
Tarek-Abdo <tarekk.abdo22.gmail.com>
2023-08-07 21:16:32 +0300

    finish the navBar component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Home } from "@mui/icons-material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Footer from "./components/Footer";
 
