52f4e77c808c74b6961869d196648c67f3b15691
Tarek-Abdo <tarekk.abdo22.gmail.com>
2023-08-10 18:43:06 +0300

    added exercises and search exercices

---
+++ src/utlis/fetchData.js
@@ -0,0 +1,15 @@
+// here where i make a connection with rapid api
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
